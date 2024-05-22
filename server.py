379f522aa386a1405e7b6c3bb207e419fc992c1c
Abeer Rozba <abeerrozba3@gmail.com>
2024-05-22 22:24:47 +0300

    Edited printing format

--- server.py
+++ server.py
@@ -5,12 +5,13 @@ server_address = ("127.0.0.1", 49158)
 
 def handle_client(client_socket, client_address):
     client = client_socket.recv(1024).decode("ascii")
-    print("New connection accepted from: ", client)
+    print(f"New connection accepted from: {client}")
     while True:
         data = client_socket.recv(1024).decode("ascii")
         if not data:
-            print(client, " disconnected.")
             break
+        print (f"Incoming request from {client} : {request}")
+    print(f"{client} disconnected.")
     client_socket.close()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
