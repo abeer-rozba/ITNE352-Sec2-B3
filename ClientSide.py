b32eda3ec468c0c555bd8f908ce28b516c5aca35
Muna <munabawaenah03@gmail.com>
2024-05-23 21:07:21 +0300

    part1

--- ClientSide.py
+++ ClientSide.py
@@ -1,1 +1,13 @@
-test 1 test
+import socket
+import threading
+import time
+
+Client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREA)
+Client_socket.connect(("127.0.0.1",49158))
+Client_socket.send("ClientSide".encode()) #Establish a connection with the server
+
+def send_receive(command, data=None):
+    Client_socket.send(command.encode())
+    if data:
+        Client_socket.send(data.encode())
+    return Client_socket.recv(1024).decode()    
